e7dcfa0c4807ec4be9a2e3df5d5d790c23791ca0
Daniel Langlois <2001802@apps.nsd.org>
2025-09-15 17:05:13 +0000

    tictactoe1

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -12,18 +12,19 @@ package edu.bhscs;
 
 public class Main {
 
+  // this is an example of a 'char' constant
+  // because it is global, it should be in capital letters
   public static final char A = 'a';
 
   public static void main(String[] args) {
     int someNumber = 67;
 
-
     System.out.println("67!" + A + someNumber); // intuitive output
     System.out.println(A + "?" + A + someNumber); // intuitive output
     System.out.println(A + A + someNumber); // unintuitive output, expected aa67; result 261; reason
-                                            // when there is not a string inn the argument, adds
-                                            // unicode character value instead of character (97 + 97
-                                            // + 67 = 261)
+    // when there is not a string inn the argument, adds
+    // unicode character value instead of character (97 + 97
+    // + 67 = 261)
     System.out.println();
     System.out.println("this is neet");
     System.out.println();
@@ -31,4 +32,3 @@ public class Main {
     System.out.println("Ok I am done");
   }
 }
-
