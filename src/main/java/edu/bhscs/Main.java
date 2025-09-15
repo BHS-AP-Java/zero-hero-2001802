3874b7838171fe6956a62337cf82c7100530b93c
Daniel Langlois <2001802@apps.nsd.org>
2025-09-15 16:36:45 +0000

    a+a+someNumber = 261

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -4,19 +4,31 @@
 // 09/12/2025
 
 /*
- * DESCRIPTION: Whatever Mr Reiber says
- * INPUT: We dont know about input yet
- * OUTPUT:Some cool words in the terminal
- * EDGE CASE: They are all edge cases
+ * DESCRIPTION: Whatever Mr Reiber says INPUT: We dont know about input yet OUTPUT:Some cool words
+ * in the terminal EDGE CASE: They are all edge cases
  */
 
 package edu.bhscs;
 
-public class Main{
-  public static void main(String[]args){
-    System.out.println("Hello, world");
-    System.out.println("Goodbye, world");
-    System.out.println("!");
-    System.out.println("Knobby knockers");
+public class Main {
+
+  public static final char A = 'a';
+
+  public static void main(String[] args) {
+    int someNumber = 67;
+
+
+    System.out.println("67!" + A + someNumber); // intuitive output
+    System.out.println(A + "?" + A + someNumber); // intuitive output
+    System.out.println(A + A + someNumber); // unintuitive output, expected aa67; result 261; reason
+                                            // when there is not a string inn the argument, adds
+                                            // unicode character value instead of character (97 + 97
+                                            // + 67 = 261)
+    System.out.println();
+    System.out.println("this is neet");
+    System.out.println();
+    System.out.println();
+    System.out.println("Ok I am done");
   }
 }
+
