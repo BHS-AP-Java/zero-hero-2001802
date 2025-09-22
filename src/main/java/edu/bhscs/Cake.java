995155cd9ea1fa2f3d3d3df992543328f49b1ec9
Daniel Langlois <2001802@apps.nsd.org>
2025-09-22 16:20:40 +0000

    cakev5 definately working

--- src/main/java/edu/bhscs/Cake.java
+++ src/main/java/edu/bhscs/Cake.java
@@ -2,20 +2,21 @@ package edu.bhscs;
 
 public class Cake {
   // I have to make a constructor
-  private String flavor;
-  private int weight;
+  public String flavor;
+  public int weight;
 
-  public Cake(String flavor, int weight){
+  public Cake(String flavor, int weight) {
     this.flavor = flavor;
     this.weight = weight;
   }
 
-  public void eaten(){
-    weight -=2;
+  public void eaten() {
+    weight -= 2;
     System.out.println("The cake has been eated there is weight of " + weight + " weight left");
+    System.out.println("I eated 2 weight of cake");
   }
 
-  public String getFlavor(){
+  public String getFlavor() {
     return flavor;
   }
 }
--- src/main/java/edu/bhscs/Person.java
+++ src/main/java/edu/bhscs/Person.java
@@ -1,10 +1,10 @@
 package edu.bhscs;
 
 public class Person {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Store store = new Store();
     Cake boughtCake = store.sellCake();
-    System.out.println(" der user has purchased a " + boughtCake.getFlavor() + "cake");
+    System.out.println("the user has purchased a " + boughtCake.getFlavor() + " cake");
     boughtCake.eaten();
   }
 }
