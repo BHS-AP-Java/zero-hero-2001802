8a942f29032fcd4ac689f8e58a7153e6e699d6c7
Daniel Langlois <2001802@apps.nsd.org>
2025-10-06 16:22:46 +0000

    ready to submit maybe not really

--- src/main/java/edu/bhscs/Baker.java
+++ src/main/java/edu/bhscs/Baker.java
@@ -1,6 +1,7 @@
 package edu.bhscs;
 
 public class Baker {
+  //not finished creating baker but uses random numbers to determine in part quality of cake
   int randomInt = (int) (Math.random() * 10) + 1;
   int temp = randomInt;
 
--- src/main/java/edu/bhscs/Flour.java
+++ src/main/java/edu/bhscs/Flour.java
@@ -1,6 +1,8 @@
 package edu.bhscs;
 
 public class Flour {
+  // uses random to create flour quality and weight
+  // quality and weight determine price
   int flourQuality = (int) (Math.random() * 10) + 1;
   int flourQuantity = (int) (Math.random() * 50) + 1;
 
