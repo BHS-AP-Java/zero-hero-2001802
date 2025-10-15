eec56f3ed2a94e285e40dbb41c9d27c460fc89e4
Daniel Langlois <2001802@apps.nsd.org>
2025-10-15 14:53:59 +0000

    proper 3d cake

--- src/main/java/edu/bhscs/CakeDrawer.java
+++ src/main/java/edu/bhscs/CakeDrawer.java
@@ -30,45 +30,6 @@ public class CakeDrawer {
   }
 
   public void cakeHWD(int width, int height, int depth) {
-    // width
-    for (int i = 0; i < width; i++) {
-      guts += "-";
-    }
-
-    // height
-
-    for( var depth2 = depth; depth2 > height; depth2--){
-      placeHolder = "";
-
-
-      for(int n = 0; n < width;n++){
-        placeHolder += "^";
-
-
-      }
-      for(var v = 0; v< depth + (depth - height); v++){
-
-        placeHolder += "/";
-        String space = " ";
-        placeHolder2 = space + "/" + placeHolder;
-        c = -1;
-
-      }
-      placeHolder3 = placeHolder2.substring(0, placeHolder2.length() -1);
-      System.out.println(placeHolder3);
-    }
-    for (int j = 0; j < height; j++) {
-      depth -= 1;
-      for (int m = 0; m <= depth; m++) {
-        cakeDepth += "/";
-        placeHolder += "/";
-
-
-      }
-
-      System.out.println(guts + cakeDepth);
-
-      cakeDepth = "";
-    }
+      
   }
 }
